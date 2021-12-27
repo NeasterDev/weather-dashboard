18f5505520b66c2c2de38f93a1990b83bb2ed544
Nicholas Easter <nicholas.easter17@gmail.com>
2021-12-26 20:13:36 -0800

    got correct lat and lon for desired city and called api with those parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,18 @@ var searchResultForm = document.querySelector(".search-result-form")
 var citySearchEl = document.querySelector("#city-search");
 var cityResultEl = document.querySelector("#result");
 var weatherHeaderEl = document.querySelector("#weather-header");
+var weatherTempEl = document.querySelector("#weather-temp");
+var weatherWindEl = document.querySelector("#weather-wind");
+var weatherHumidityEl = document.querySelector("#weather-humidity");
+var weatherUvEl = document.querySelector("#weather-uv");
+
+var appId = "ef8fa03f5832204a6c4c00587e87395b";
+
 
 citySearchFormEl.addEventListener("submit", function(event){
     event.preventDefault();
     cityName = citySearchEl.value;
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=ef8fa03f5832204a6c4c00587e87395b";
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=ef8fa03f5832204a6c4c00587e87395b";
     var datas = {};
     fetch(url)
     .then(function(result) {
@@ -35,16 +42,33 @@ citySearchFormEl.addEventListener("submit", function(event){
 searchResultForm.addEventListener("click", function(event) {
     event.preventDefault();
     if(event.target.value) {
-        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + event.target.value + "&appid=ef8fa03f5832204a6c4c00587e87395b";
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + event.target.value + "&units=imperial&appid=" + appId;
+        var newUrl = "";
         var datas = {};
-        fetch(url)
-        .then(function(result) {
+        var lat, lon = "";
+
+        fetch(url).then(function(result) {
             return result.json();
         }).then(function(data) {
-            datas = data;
-        }).then(function() {
-            console.log(datas);
-            weatherHeaderEl.textContent = JSON.stringify(datas.name).replaceAll('"', "");
+            console.log(data);
+            lat = JSON.stringify(data.coord.lat);
+            lon = JSON.stringify(data.coord.lon);
+        }).then(function(){
+            console.log("LAT: " + lat + "  LON: " + lon);
+            newUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=ef8fa03f5832204a6c4c00587e87395b";
+        }).then(function(){
+            fetch(newUrl).then(function(result) {
+                return result.json();
+            }).then(function(data) {
+                datas = data;
+            }).then(function() {
+                console.log(datas);
+                weatherHeaderEl.textContent = event.target.value;
+                weatherTempEl.textContent = "Temp: " + JSON.stringify(datas.current.temp) + " \xB0" + "F";
+                weatherWindEl.textContent = "Wind: " + JSON.stringify(datas.current.wind_speed) + " mph";
+                weatherHumidityEl.textContent = "Humidity: " + JSON.stringify(datas.current.humidity) + "%";
+                weatherUvEl.textContent = "UV Index: " + JSON.stringify(datas.current.uvi);
+            });
         });
     }
 });
