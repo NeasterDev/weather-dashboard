a7fdcf2112f3de3861ca171058828c5caaaffd8b
Nicholas Easter <nicholas.easter17@gmail.com>
2021-12-29 11:38:37 -0800

    fixed problem where multiple searches would result in multiple 5 day forecasts on the same page

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,10 @@ citySearchFormEl.addEventListener("submit", function(event){
     event.preventDefault();
     cityName = citySearchEl.value.split(" ");
     
+    while(forecastInfoEl.firstChild) {
+        forecastInfoEl.removeChild(forecastInfoEl.firstChild);
+    }
+
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=ef8fa03f5832204a6c4c00587e87395b";
     var newUrl = "";
     var datas = {};
@@ -139,6 +143,10 @@ citySearchFormEl.addEventListener("submit", function(event){
 
 searchResultFormEl.addEventListener("click", function(event) {
     event.preventDefault();
+
+    while(forecastInfoEl.firstChild) {
+        forecastInfoEl.removeChild(forecastInfoEl.firstChild);
+    }
     if(event.target.value) {
         var input = event.target.value.split(" ");
         console.log("0" + input[0]);
@@ -169,8 +177,66 @@ searchResultFormEl.addEventListener("click", function(event) {
                 weatherWindEl.textContent = "Wind: " + JSON.stringify(datas.current.wind_speed) + " mph";
                 weatherHumidityEl.textContent = "Humidity: " + JSON.stringify(datas.current.humidity) + "%";
                 weatherUvEl.textContent = "UV Index: " + JSON.stringify(datas.current.uvi);
+            
+                var currentDate = new Date();
+                var currentMonth = new Date();
+                var monthCounter = currentMonth.getMonth();
+                monthCounter = monthCounter + 1;
+                var currentYear = new Date();
+                console.log(datas);
+                for(var i = 0; i < 5; i++) {
+                    
+                    var iconUrl = "http://openweathermap.org/img/wn/" + JSON.stringify(datas.daily[i].weather[0].icon).replaceAll('"', "") +"@2x.png";
+                    
+                    // create new forecast box
+                    var forecastBoxEl = document.createElement("div");
+                    forecastBoxEl.setAttribute("class", "forecast-box");
+
+                    // create new date 
+                    var forecastDateEl = document.createElement('h3');
+                    forecastDateEl.setAttribute("class", "forecast-date");
+                    forecastDateEl.textContent = monthCounter + "/" + currentDate.getDate() + "/" + currentYear.getFullYear();
+                    currentDate.setDate(currentDate.getDate() + 1);
+
+                    // create new span to hold icon
+                    var forecastIconSpanEl = document.createElement("span");
+                    forecastIconSpanEl.setAttribute("class", "forecast-icon-span");
+
+                    // create new img tag for the icon
+                    var forecastIconEl = document.createElement('img');
+                    forecastIconEl.setAttribute('class', 'forecast-icon');
+
+                    // create temperature tag
+                    var tempEl = document.createElement("h4");
+                    tempEl.textContent = datas.daily[i].temp.day + "\xB0" + " f";
+
+                    // create wind tag
+                    var windEl = document.createElement("h4");
+                    windEl.textContent = "Wind: " + datas.daily[i].wind_speed + " mph";
+
+                    // create humidity tag
+                    var humidityEl = document.createElement("h4");
+                    humidityEl.textContent = "Humidity: " + datas.daily[i].humidity + "%";
+                    
+                    console.log(iconUrl);
+                    forecastIconEl.setAttribute('src', iconUrl);
+
+                    // append the forecast box to the screen
+                    forecastInfoEl.append(forecastBoxEl);
+
+                    // append date to box
+                    forecastBoxEl.append(forecastDateEl);
+
+                    // append span and img to box
+                    forecastBoxEl.append(forecastIconSpanEl);
+                    forecastIconSpanEl.append(forecastIconEl);
+
+                    // append temp/wind/humidity to box
+                    forecastBoxEl.append(tempEl);   
+                    forecastBoxEl.append(windEl);
+                    forecastBoxEl.append(humidityEl);
+                } 
             });
         });
     }
 });
-
